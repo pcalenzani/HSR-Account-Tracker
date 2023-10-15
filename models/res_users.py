e1cc2e8c24ec39b97f014365aa1b3111708252f1
inkblots <asmodeus937@gmail.com>
2023-10-15 09:50:59 -0700

    added initial warp functionality

--- models/res_users.py
+++ models/res_users.py
@@ -2,27 +2,51 @@ from odoo import api, fields, models, tools
 from urllib.parse import urlencode, parse_qs
 import requests
 
-WARP_API_URL = "https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog"
+WARP_API_URL = 'https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog'
 
 class Users(models.Model):
-    _description = "Warp"
-    _inherit = "res.users"
+    _description = 'Player'
+    _inherit = 'res.users'
 
-    sr_uid = fields.Char(string="Star Rail UID")
-    sr_authkey = fields.Char(string="Auth Key")
+    sr_uid = fields.Char('Star Rail UID')
+    sr_authkey = fields.Char('Auth Key')
+    sr_update = fields.Boolean('Update Player Warps', default=False)
 
-    def getAuthKeyFromUrl(self, url):
-        self.sr_authkey = parse_qs(url)["authkey"]
+    def get_authkey_from_url(self, url):
+        self.sr_authkey = parse_qs(url)['authkey']
 
-    def getWarpData(self, gacha_type):
+    def get_warp_data(self, gacha_type, size=20, end_id=0):
+        self.ensure_one()
+
         params = {
             'authkey_ver': '1',
             'lang': 'en',
             'authkey': self.sr_authkey,
             'game_biz': 'hkrpg_global',
-            'size': '20',
+            'size': size,
             'gacha_type': gacha_type,
-            'end_id': '0' # TODO
+            'end_id': end_id
         }
-        return requests.get(WARP_API_URL + "?" + urlencode(params)).json()
+        ret = requests.get(WARP_API_URL + '?' + urlencode(params)).json()
+        
+        # TODO Log or raise this
+        if ret['retcode'] != 0:
+            print(f"Error {ret['retcode']}: {ret['message']}") 
+        return ret['data']
+    
+    def get_warps(self):
+        # banner_types = self.env['sr.banner.type'].search().mapped('banner_key')
+        # 1 norm
+        # 2 departure
+        # 11 special
+        # 12 lc
+        banner_types = ['2'] # Debug
 
+        for type in banner_types:
+            end_id = 0
+            while end_id is not None:
+                data = self.get_warp_data(type, end_id=end_id)
+                if data['size'] == '0':
+                    break
+
+                end_id = self.env['sr.warp'].generate_warps(data['list'])
--- models/warp.py
+++ models/warp.py
@@ -1,17 +1,75 @@
 from odoo import api, fields, models, tools
+from odoo.exceptions import ValidationError
+from datetime import datetime, timedelta
 
 class Warp(models.Model):
-    _name = "sr.warp"
-    _description = "Warp"
+    _name = 'sr.warp'
+    _description = 'Warp'
 
-    uid = fields.Char(string="User ID")
-    gacha_id = fields.Char(string="Banner ID")
-    gacha_type = fields.Char(string="Banner Type")
-    item_id = fields.Char(string="Item ID")
-    count = fields.Char(string="Count")
-    time = fields.Char(string="Time")
-    name = fields.Char(string="Name")
-    lang = fields.Char(string="Lang")
-    item_type = fields.Char(string="Item Type")
-    rank_type = fields.Char(string="Rarity")
-    wid = fields.Char(string="Warp ID")
+    uid = fields.Char('User ID')
+    gacha_id = fields.Char('Banner ID')
+    gacha_type = fields.Char('Banner Type')
+    item_id = fields.Char('Item ID')
+    count = fields.Char('Count')
+    time = fields.Datetime('Time')
+    name = fields.Char('Name')
+    lang = fields.Char('Lang')
+    item_type = fields.Char('Item Type')
+    rank_type = fields.Integer('Rarity')
+    wid = fields.Char('Warp ID', index=True)
+
+    banner_id = fields.Many2one('sr.banner', compute='_compute_warp_banner')
+
+    _sql_constraints = [
+        ('warp_key', 'UNIQUE (wid)',  'You can not have two warps with the same ID')
+    ]
+
+    def _compute_warp_banner(self):
+        sr_banner = self.env['sr.banner'].search([('gacha_id','=',self.gacha_id)])
+        if not sr_banner:
+            sr_banner = self.env['sr.banner'].create({
+                'banner_key': self.gacha_id,
+                'gacha_type_id': self.gacha_type,
+            })
+        self.banner_id = sr_banner
+
+    def generate_warps(self, vals_list):
+        try:
+            warps = self.create(vals_list)
+            return warps[-1]['wid']
+        except ValidationError:
+            return None
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'time' in vals:
+                vals['time'] = datetime.strptime(vals['time'], "%Y-%m-%d %H:%M:%S") + timedelta(hours=+5)
+        
+        return super(Warp, self).create(vals_list)
+
+
+
+class BannerType(models.Model):
+    _name = 'sr.banner.type'
+    _description = 'Warp Banner Type'
+
+    name = fields.Char('Name')
+    gacha_type = fields.Char('Banner Type')
+
+
+class Banner(models.Model):
+    _name = 'sr.banner'
+    _description = 'Warp Banner'
+
+    name = fields.Char('Name', default='~')
+    banner_key = fields.Char('Banner ID')
+    gacha_type_id = fields.Many2one('sr.banner.type')
+
+    _sql_constraints = [
+        ('banner_key', 'UNIQUE (banner_key)',  'You can not have two banners with the same key')
+    ]
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        return super(Banner, self).create(vals_list)
