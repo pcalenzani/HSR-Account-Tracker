ca210e2b847b347ea5cbaef230b2744c36454f8d
inkblots <asmodeus937@gmail.com>
2023-10-14 22:44:02 -0700

    warp skeleton

---
+++ models/warp.py
@@ -0,0 +1,15 @@
+class Warp(models.Model):
+    _name = "sr.warp"
+    _description = "Warp"
+
+    uid = fields.Char(string="User ID")
+    gacha_id = fields.Char(string="Banner ID")
+    gacha_type = fields.Char(string="Banner Type")
+    item_id = fields.Char(string="Item ID")
+    count = fields.Char(string="Count")
+    time = fields.Char(string="Time")
+    name = fields.Char(string="Name")
+    lang = fields.Char(string="Lang")
+    item_type = fields.Char(string="Item Type")
+    rank_type = fields.Char(string="Rarity")
+    wid = fields.Char(string="Warp ID")
