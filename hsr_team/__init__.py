bd9374f193da46be7ab744d92ecef2165b3c21b8
inkblots <asmodeus937@gmail.com>
2023-10-22 22:39:02 -0700

    pre init correct tables

--- hsr_team/__init__.py
+++ hsr_team/__init__.py
@@ -1,5 +1,6 @@
 from . import models
 
 def pre_init(cr):
-    cr.execute("""CREATE TABLE sr_character_template (id serial, primary key(id));""")
-    cr.execute("""CREATE TABLE sr_character (primary key(id)) INHERITS (sr_character_template);""")
+    cr.execute("""CREATE TABLE sr_item (id serial, primary key(id));""")
+    cr.execute("""CREATE TABLE sr_character (primary key(id)) INHERITS (sr_item);""")
+    cr.execute("""CREATE TABLE sr_light_cone (primary key(id)) INHERITS (sr_item);""")
