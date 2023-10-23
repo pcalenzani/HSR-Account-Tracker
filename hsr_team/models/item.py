9bbc504c76348722b48d198135e780fadaad65e7
inkblots <asmodeus937@gmail.com>
2023-10-22 22:48:41 -0700

    compute count for item and add free pulls

--- hsr_team/models/item.py
+++ hsr_team/models/item.py
@@ -6,9 +6,11 @@ class Item(models.Model):
     _order = 'item_id DESC'
 
     # --- Manual Fields ---
-    count = fields.Integer('Count', compute='_compute_count')
-    is_owned = fields.Boolean('Is Owned')
+    count = fields.Integer('Count', store=True, compute='_compute_count')
+    is_owned = fields.Boolean('Is Owned', store=True, compute='_compute_count')
     date_obtained = fields.Date('Obtained on')
+    free_pulls = fields.Integer('Free Pulls')
+    warp_ids = fields.Many2many('sr.warp')
 
     # --- API Fields ---
     item_id = fields.Integer('Item ID')
@@ -31,8 +33,15 @@ class Item(models.Model):
         string='Path'
     )
 
+    _sql_constraints = [
+        ('item_key', 'UNIQUE (item_id)',  'Duplicate deteced. Item ID must be unique.')
+    ]
+    
+    @api.depends('warp_ids')
     def _compute_count(self):
-        self.count = self.env['sr.warp'].search_count([('item_id','=',str(self.item_id))])
+        item_count = self.env['sr.warp'].search_count([('item_id','=',str(self.item_id))])
+        self.count = item_count
+        self.is_owned = item_count + self.free_pulls
 
 
 class LightCone(models.Model):
