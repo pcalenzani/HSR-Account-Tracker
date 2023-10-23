6989e6720615793e1a8ed6d1f0874d9143dc082d
inkblots <asmodeus937@gmail.com>
2023-10-22 22:35:39 -0700

    move code to module init file

--- hsr_team/__init__.py
+++ hsr_team/__init__.py
@@ -1,1 +1,5 @@
 from . import models
+
+def pre_init(cr):
+    cr.execute("""CREATE TABLE sr_character_template (id serial, primary key(id));""")
+    cr.execute("""CREATE TABLE sr_character (primary key(id)) INHERITS (sr_character_template);""")
--- hsr_team/models/__init__.py
+++ hsr_team/models/__init__.py
@@ -1,7 +1,3 @@
 from . import item
 from . import relic
 from . import character
-
-def pre_init(cr):
-    cr.execute("""CREATE TABLE sr_character_template (id serial, primary key(id));""")
-    cr.execute("""CREATE TABLE sr_character (primary key(id)) INHERITS (sr_character_template);""")
