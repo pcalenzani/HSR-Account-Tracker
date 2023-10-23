8f98e60596b9ac2fa5f698e7ef3615d35c66797d
inkblots <asmodeus937@gmail.com>
2023-10-22 18:59:03 -0700

    change tab title asset

--- hsr_warp/static/src/web_client.js
+++ hsr_warp/static/src/web_client.js
@@ -1,11 +1,13 @@
-/** @odoo-module alias=web.window.title **/
+/** @odoo-module **/
 
 import { WebClient } from "@web/webclient/webclient";
-import {patch} from "@web/core/utils/patch";
+import { patch } from "@web/core/utils/patch";
+import { session } from "@web/session";
 
-patch(WebClient.prototype, "Web Window Title", {
-    setup() {
-        this._super();
-        this.title.setParts({ zopenerp: 'Star Rail' });
-    }
+patch(WebClient.prototype, "Your custom module.WebClient", {
+setup() {
+this._super.apply(this, arguments);
+const app_system_name = 'Star Rail';
+this.title.setParts({ zopenerp: app_system_name }); // zopenerp is easy to grep
+}
 });
