3b591a147dd46fec010aefb7416878db88c2f4f3
inkblots <asmodeus937@gmail.com>
2023-10-24 18:12:41 -0700

    debug

--- hsr_team/models/character.py
+++ hsr_team/models/character.py
@@ -76,7 +76,7 @@ class Eidolon(models.Model):
         ]
     )
     owned = fields.Boolean('Is Owned')
-    character_template_id = fields.Many2one('sr.character.template', string='Character',store=True)
+    character_template_id = fields.Many2one('sr.character.template', 'eidolon_ids', string='Character',store=True)
     character_item_id = fields.Integer(related='character_template_id.character_id')
 
 
