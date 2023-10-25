97943e533a6ed2719b21386191a0511335371b46
inkblots <asmodeus937@gmail.com>
2023-10-24 23:38:45 -0700

    debug

--- hsr_team/models/item.py
+++ hsr_team/models/item.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 sys.path.append(os.path.expanduser('~') + '/src/starrail')
-from hsr_warp.sr_tools import get_image_data
+import sr_tools
 
 _logger = logging.getLogger(__name__)
 
@@ -177,7 +177,7 @@ class Material(models.Model):
     @api.depends('img_path')
     def _compute_image(self):
         for rec in self:
-            rec.image = get_image_data(self.img_path)
+            rec.image = sr_tools.get_image_data(self.img_path)
 
     @api.model_create_multi
     def create(self, vals_list):
