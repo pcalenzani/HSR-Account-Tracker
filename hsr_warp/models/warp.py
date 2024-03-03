d8bcd646ee25aabc75b97de5cc518bf2af5f86f6
inkblots <asmodeus937@gmail.com>
2024-03-02 21:29:10 -0800

    test browse

--- hsr_warp/models/warp.py
+++ hsr_warp/models/warp.py
@@ -85,7 +85,7 @@ class Warp(models.Model):
             sr_ids= tuple(sr_ids)
 
         self.env.cr.execute("""SELECT id FROM sr_warp WHERE wid in %s""", [sr_ids])
-        ids = self.env.cr.fetchall()
+        ids = self.env.cr.fetchall()[0]
         _logger.info(ids)
         return self.__class__(self.env, ids, ids)
 
