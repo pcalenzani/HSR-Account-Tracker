d68c41a371183ab88788f00c7a631e32c638a37a
inkblots <asmodeus937@gmail.com>
2024-03-04 22:15:14 -0800

    oops bad replace

--- hsr_team/__init__.py
+++ hsr_team/__init__.py
@@ -1,8 +1,8 @@
 from . import models
 
 def pre_init(env):
-    env.cr.execute("""env.crEATE TABLE sr_item (id serial, primary key(id));""")
-    env.cr.execute("""env.crEATE TABLE sr_character (primary key(id)) INHERITS (sr_item);""")
-    env.cr.execute("""env.crEATE TABLE sr_light_cone (primary key(id)) INHERITS (sr_item);""")
-    env.cr.execute("""env.crEATE TABLE sr_item_material (primary key(id)) INHERITS (sr_item);""")
-    env.cr.execute("""env.crEATE TABLE sr_relic (primary key(id)) INHERITS (sr_item);""")
+    env.cr.execute("""CREATE TABLE sr_item (id serial, primary key(id));""")
+    env.cr.execute("""CREATE TABLE sr_character (primary key(id)) INHERITS (sr_item);""")
+    env.cr.execute("""CREATE TABLE sr_light_cone (primary key(id)) INHERITS (sr_item);""")
+    env.cr.execute("""CREATE TABLE sr_item_material (primary key(id)) INHERITS (sr_item);""")
+    env.cr.execute("""CREATE TABLE sr_relic (primary key(id)) INHERITS (sr_item);""")
