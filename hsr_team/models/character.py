5bf8cfbba4130b375c0a4c6abb9b6296acc03a06
inkblots <asmodeus937@gmail.com>
2024-03-05 16:46:13 -0800

    fixed parameters for mixin

--- hsr_team/models/character.py
+++ hsr_team/models/character.py
@@ -113,7 +113,7 @@ class Element(models.Model):
             if 'name' in vals:
                 # Generate image attachment
                 img_path = '/hsr_warp/static/icon/element/'
-                vals['img_id'] = self.generate_image(img_path, name=vals['name']).id
+                vals['img_id'] = self.generate_image(img_path, vals['name']).id
         return super(Element, self).create(vals_list)
     
 
@@ -143,7 +143,7 @@ class Path(models.Model):
             if 'name' in vals:
                 # Generate image attachment
                 img_path = '/hsr_warp/static/icon/path/'
-                vals['img_id'] = self.generate_image(img_path, name=vals['name']).id
+                vals['img_id'] = self.generate_image(img_path, vals['name']).id
         return super(Element, self).create(vals_list)
     
 
