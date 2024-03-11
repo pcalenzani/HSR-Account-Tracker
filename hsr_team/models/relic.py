286852e89338a1d049ea1a108b3394232f4b76f5
inkblots <asmodeus937@gmail.com>
2024-03-10 23:13:22 -0700

    set up relic and relic set models

--- hsr_team/models/relic.py
+++ hsr_team/models/relic.py
@@ -1,33 +1,28 @@
 from odoo import api, fields, models, Command
 
-class RelicSetBonus(models.Model):
-    _name = 'sr.relic.set.bonus'
-    _description = 'Relic Bonus'
-
-    # --- Manual Fields ---
-    relic_set_id = fields.Many2one('sr.relic.set')
-
-    # --- API Fields ---
-    num = fields.Char('Bonus Requirement')
-    desc = fields.Char('Description')
-
-    def name_get(self):
-        return [(rec.id, f"{rec.num} Set - {rec.relic_set_id.name}") for rec in self]
-    
     
 class RelicSet(models.Model):
     _name = 'sr.relic.set'
     _description = 'Relic Set'
+    _inherit = 'sr.image.mixin'
 
-    # --- Manual Fields ---
-    is_ornament = fields.Boolean('Is Ornament Set?')
-    is_relic_set = fields.Boolean('Is Relic Set?')
-
-    bonus_ids = fields.One2many('sr.relic.set.bonus', inverse_name='relic_set_id', string='Set Bonuses')
-
-    # --- API Fields ---
-    name = fields.Char('Set')
     set_id = fields.Integer('Set ID')
+    name = fields.Char('Set')
+
+    is_ornament = fields.Boolean('Ornament Set')
+    is_relic = fields.Boolean('Relic Set')
+    bonus_two = fields.Char('2-Set Bonus')
+    bonus_four = fields.Char('4-Set Bonus')
+    
+    img_id = fields.Many2one('ir.attachment', string='Image', compute='_compute_img_id')
+
+    @api.depends('set_id')
+    def _compute_img_id(self):
+        # Get image attachment when updating item_id
+        # Icon format will be 'icon/relic/116.png'
+        img_path = '/hsr_warp/static/icon/relic/'
+        for rec in self:
+            rec.img_id = rec.get_image_from_path(img_path + rec.set_id).id
 
 
 class Relic(models.Model):
@@ -39,51 +34,50 @@ class Relic(models.Model):
     score = fields.Float('Relic Score')
 
     # --- API Fields ---
-    name = fields.Char('Relic Name')
     set_id = fields.Many2one('sr.relic.set')
-    main_affix = fields.Many2one('sr.relic.affix', string='Main Attribute')
-    sub_affix = fields.Many2many('sr.relic.affix', string='Attributes')
+
+    main_affix_id = fields.Many2one('sr.attribute')
+    sub_affix_ids = fields.One2Many('sr.attribute', 'relic_id')
+
+    icon = fields.Char('Icon Image Path')
+    img_id = fields.Many2one('ir.attachment', string='Image', compute='_compute_img_id')
 
     def name_get(self):
         return [(rec.id, f"{rec.set_id.name}: {rec.relic_name}") for rec in self]
+    
+    @api.depends('icon')
+    def _compute_img_id(self):
+        # Get image attachment when updating item_id
+        # Icon format will be 'icon/relic/116_2.png'
+        img_path = '/hsr_warp/static/'
+        for rec in self:
+            rec.img_id = rec.get_image_from_path(img_path + rec.icon).id
 
+    def _populate_relics(self, data):
+        '''
+            id          - Object ID, str
+            name        - Name of relic, str
+            set_id      - Relic set ID, str
+            set_name    - Relic set name, str
+            rarity      - Star Rarity, int
+            level       - Relic Level, int
+            icon        - Relic Image Path, str
+            main_affix  - Primary Relic Stat, dict
+            sub_affix   - Relic Substats, list(dict)
+        '''
+        to_remove = ['set_name']
+        for rel in data:
+            # Remove key if exists
+            for k in to_remove: rel.pop(k, None) 
+            # Rename id key to db friendly, cast to int for lookup
+            rel['item_id'] = int(rel.pop('id'))
+            # Typecast fields for easy storing
+            rel['rarity'] = str(rel.pop('rarity'))
 
-class RelicAffix(models.Model):
-    _name = 'sr.relic.affix'
-    _description = 'Relic Stat'
+            rel['set_id'] = self.env.ref('hsr_team.relic_set_' + rel.pop('set_id')).id
 
-    # --- Manual Fields ---
+            # TODO convert affixes into sr.attribute Commands
 
-    # --- API Fields ---
-    type = fields.Char('Attribute Type')
-    affix_name = fields.Char('Relic Attribute')
-    value = fields.Float('Value')
-    display = fields.Char(('Attribute Display'))
-    percent = fields.Boolean('Is Percent Amount')
-    count = fields.Integer('Affix Count')
-
-    field = fields.Selection(
-        selection=[
-            ('atk', 'Attack'),
-            ('def', 'Defense'),
-            ('hp', 'Health'),
-            ('crit_rate', 'Crit Rate'),
-            ('crit_dmg', 'Crit Damage'),
-            ('spd', 'Speed'),
-            ('effect_res', 'Effect Res'),
-            ('effect_hit', 'Effect Hit Rate'),
-            ('break_dmg', 'Break Effect'),
-            ('sp_rate', 'Energy Regeneration Rate'),
-            ('heal_rate', 'Outgoing Healing Boost'),
-            ('quantum_dmg', 'Quantum DMG Boost'),
-            ('imaginary_dmg', 'Imaginary DMG Boost'),
-            ('lightning_dmg', 'Lightning DMG Boost'),
-            ('physical_dmg', 'Physical DMG Boost'),
-            ('fire_dmg', 'Fire DMG Boost'),
-            ('wind_dmg', 'Wind DMG Boost'),
-            ('ice_dmg', 'Ice DMG Boost'),
-        ],
-        string='Type')
-
-    def name_get(self):
-        return [(rec.id, f"{rec.display} {rec.affix_name}") for rec in self]
+        # TODO convert data to Command
+        return data
+    
