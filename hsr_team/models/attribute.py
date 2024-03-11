7bf975b0a9975108e524110c8d2a66ad0b99172f
inkblots <asmodeus937@gmail.com>
2024-03-10 18:46:32 -0700

    odoo float round lol

--- hsr_team/models/attribute.py
+++ hsr_team/models/attribute.py
@@ -37,7 +37,7 @@ class Attribute(models.Model):
             if rec.percent:
                 val *= 100
                 tag = '%'
-            rec.display_name = str(float_round(val, precision_digits=1)) + tag
+            rec.display_name = str(float_round(val, precision_digits=2)) + tag
     
     @api.depends('icon')
     def _compute_img_id(self):
