e6105df89582f2f1665acbfea1ae035edae2e57d
inkblots <asmodeus937@gmail.com>
2024-03-31 21:57:17 -0700

    change attribute to show full name

--- hsr_team/models/attribute.py
+++ hsr_team/models/attribute.py
@@ -58,7 +58,7 @@ class Attribute(models.Model):
             if rec.percent:
                 tag = '%'
             # Don't use float_round in str() due to python float multiplication
-            rec.display_name = str(round(rec.value, 1)) + tag
+            rec.display_name = str(round(rec.value, 1)) + tag + rec.name
     
     @api.depends('icon')
     def _compute_img_id(self):
